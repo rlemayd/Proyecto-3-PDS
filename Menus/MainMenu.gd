8b7b23b00a63faa727840c812bb583314902ffb8
Track-tor <48454411+Track-tor@users.noreply.github.com>
2020-10-04 13:53:33 -0300

    buttons added

--- Menus/MainMenu.gd
+++ Menus/MainMenu.gd
@@ -1,8 +1,10 @@
 extends Control
 
-func _on_LogInButton_pressed() -> void:
-	get_tree().change_scene("res://Menus/LogInMenu.tscn")
 
 
 func _on_RegisterButton_pressed() -> void:
 	get_tree().change_scene("res://Menus/RegisterMenu.tscn")
+
+
+func _on_LoginButton_pressed():
+	get_tree().change_scene("res://Menus/LogInMenu.tscn")
