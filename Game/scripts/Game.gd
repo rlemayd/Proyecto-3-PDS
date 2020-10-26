4c959552d42f5ab1c5bb7e27a855eba0c829baf3
Track-tor <48454411+Track-tor@users.noreply.github.com>
2020-10-25 23:39:12 -0300

    Update Game.gd

--- Game/scripts/Game.gd
+++ Game/scripts/Game.gd
@@ -31,7 +31,7 @@ func _ready():
 	loadPlayers()
 	loadMyself()
 	
-	if(int(Background.currentColor) == 1):
+	if(int(Background.currentColor) == 1 and !gameStarted):
 		button.disabled = false
 		button.visible = true
 	button.set_position(Vector2(-456,-156))
@@ -149,7 +149,7 @@ func endTurn():
 	Background.currentGameData["currentTurn"]["integerValue"] = (int(Background.currentGameData["currentTurn"]["integerValue"]) % int(Background.currentGameData["playerQuantity"]["integerValue"])) + 1
 	Background.currentGameData[Background.cellColors[int(Background.currentColor)]]["integerValue"] = int(Background.currentGameData[Background.cellColors[int(Background.currentColor)]]["integerValue"]) + 1
 	Background.currentGameData[Background.cellColors[int(pastColor)]]["integerValue"] = int(Background.currentGameData[Background.cellColors[int(pastColor)]]["integerValue"]) - 1
-	if int(Background.currentGameData[Background.cellColors[1]]["integerValue"]) + int(Background.currentGameData[Background.cellColors[2]]["integerValue"]) + int(Background.currentGameData[Background.cellColors[3]]["integerValue"]) + int(Background.currentGameData[Background.cellColors[4]]["integerValue"]) + int(Background.currentGameData[Background.cellColors[5]]["integerValue"]) == Background.currentGameData["totalCells"]["integerValue"]:
+	if int(Background.currentGameData[Background.cellColors[1]]["integerValue"]) + int(Background.currentGameData[Background.cellColors[2]]["integerValue"]) + int(Background.currentGameData[Background.cellColors[3]]["integerValue"]) + int(Background.currentGameData[Background.cellColors[4]]["integerValue"]) + int(Background.currentGameData[Background.cellColors[5]]["integerValue"]) == int(Background.currentGameData["totalCells"]["integerValue"]):
 		print("Juego terminado")
 	request = "end_turn"
 	FireBase.update_document("Games/%s/Map/Info" % Background.currentGameCode, Background.currentGameData, http)
